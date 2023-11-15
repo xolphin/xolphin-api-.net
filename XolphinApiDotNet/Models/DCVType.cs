7535c077ed2b03982d43191e47862cdccfb1f8ea
Tamas Jan <tamas.jan@sectigo.com>
2023-11-15 12:01:47 +0000

    use enum converter

--- XolphinApiDotNet/Models/DCVType.cs
+++ XolphinApiDotNet/Models/DCVType.cs
@@ -1,10 +1,14 @@
-﻿namespace XolphinApiDotNet.Models
+﻿using Newtonsoft.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace XolphinApiDotNet.Models
 {
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum DCVType
     {
         EMAIL,
         FILE,
-        DNS,
-        MIXED
+        DNS
     }
 }
--- XolphinApiDotNet/Models/Response/DCVDomains.cs
+++ XolphinApiDotNet/Models/Response/DCVDomains.cs
@@ -6,7 +6,7 @@
         public bool Status { get; set; }
         public int StatusDetail { get; set; }
         public string StatusMessage { get; set; }
-        public string DCVType { get; set; }
+        public DCVType DCVType { get; set; }
         public string DCVEmail { get; set; }
         public string Md5 { get; set; }
         public string Sha1 { get; set; }
